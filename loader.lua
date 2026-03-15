13ac8b38cb9d8613b09594f95f9919692dc7305b
tonbay1 <54112293+tonbay1@users.noreply.github.com>
2026-03-15 10:49:03 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,11 +1,8 @@
 local url = "https://raw.githubusercontent.com/tonbay1/Sailor-piece/main/Sailor-piece.lua"
 
-local success, result = pcall(function()
-    return game:HttpGet(url)
-end)
+local script = game:HttpGet(url)
 
-if success then
-    loadstring(result)()
-else
-    warn("Load script failed:", result)
+local run = loadstring(script)
+if run then
+    run()
 end
